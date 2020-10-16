43ee24286c2dafc2bc116c063ef8a14562c1ccd3
James Nicholas Tomashot <ctomashot@gmail.com>
2020-10-16 23:16:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  low = name_hash[0][0]
   name_hash.each do |key, value|
+    low 
     if value < low
       low = value
       low_key = key
